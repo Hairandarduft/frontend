966459384f40c19af0519058039fbd4d5c6a49ee
Nawelle Polin <nawellepolin@MacBook-Air-de-Nawelle.local>
2024-10-07 15:06:32 +0200

    [add] analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={poppins.className}>
         <main className="flex min-h-screen flex-col">{children}</main>
+        <Analytics />
       </body>
     </html>
   )
