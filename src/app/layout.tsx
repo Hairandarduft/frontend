a27d858398fe2539d56f0d3fd68e0d76b90c55b2
Nawelle Polin <nawellepolin@MacBook-Air-de-Nawelle.local>
2024-10-10 10:38:33 +0200

    [add] metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: 'hairandarduft',
-  description: 'Fresh bread daily',
+  description: 'Best Andarduft shampoo in the world',
 }
 
 export default function RootLayout({
